89e71720a5dd1ca182bd9bda3e4483016aaa91a5
Dim0chka <Bugrick2004@yandex.ru>
2023-10-02 19:20:43 +0300

    new

---
+++ components/AppHeader.vue
@@ -0,0 +1,279 @@
+<template>
+
+    <header class="header">
+        <div class="container">
+            <div class="header-flex">
+                <nuxt-link class="logo" to="/">
+                    <img src="/logo/logo.svg" alt="">
+                </nuxt-link>
+
+                <div @click="isActive = !isActive" :class="{'bg-btn': isActive}" class="menu-btn">
+                    <div class="part1"></div>
+                    <div class="part2"></div>
+                    <div class="part3"></div>
+                </div>
+
+                <div class="head-info">
+        
+                    <div class="side-icon-box" v-for="box in headInfo">
+                        <div class="icon">
+                            <img :src="'/logo/' + box.icon " alt="">
+                        </div>
+                        <div class="content">
+                            <span>{{ box.title }}</span>
+                            <nuxt-link :to="box.to">
+                                {{ box.text }}
+                            </nuxt-link>
+                        </div>
+                    </div>
+        
+                </div>
+            </div>
+            <nav class="header-nav">
+                <nuxt-link class="header-nav-item" v-for="nav in headNav" :to="nav.to">
+                    {{ nav.name }}
+                </nuxt-link>
+            </nav>
+        </div>
+        <transition name="appear">
+                <nav v-show="isActive" class="header-mob-nav">
+                    <div class="container">
+                        <div class="menu header-mob-flex">
+                            <nuxt-link class="header-mob-nav-item" v-for="nav in headNav" :to="nav.to">
+                                {{ nav.name }}
+                            </nuxt-link>
+                        </div>
+                    </div>
+                </nav>
+        </transition>
+    </header>
+</template>
+
+<script setup>
+
+const isActive = ref(false)
+
+
+const headInfo = [
+    {icon: "home.svg", title: "Адрес", text: "ул.Куусинена, д.21Б, оф.318", to: "https://yandex.ru/maps/-/CDU9UMLk"},
+    {icon: "phone.svg", title: "Позвонить нам", text: "+7 (495) 783-68-53", to: "tel:+74957836853"},
+    {icon: "envelope.svg", title: "Эл. почта", text: "info@vzdv.ru", to: "malito:info@vzdv.ru"}
+]
+
+const headNav = [
+    {name: "Главная", to: "/"},
+    {name: "Проекты", to: "/"},
+    {name: "Документы", to: "/"},
+    {name: "О компании", to: "/"},
+    {name: "Контакты", to: "/"}
+]
+
+</script>
+
+<style lang="scss">
+.header {
+    background-color: $text-header;
+    position: sticky;   
+    top: 0;
+    width: 100%;
+    left : 0;
+    z-index : 10;
+
+    @media only screen and (min-width: 1280px) {
+        position: relative;
+    }
+}
+
+.appear-enter-active {
+    animation: showMob .5s;
+}
+
+.appear-leave-active {
+    animation: hidMob .5s;
+}
+
+@keyframes showMob {
+    0% {
+        max-height: 0;
+    }
+    100% {
+        max-height: 100%;
+    }
+}
+
+@keyframes hidMob {
+    0% {
+        max-height: 100%;
+    }
+    100% {
+        max-height: 0;
+    }
+}
+
+.header-mob-nav {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    overflow: hidden;
+    
+
+    background-color: $bg-header;
+
+    @media only screen and (min-width: 1280px) {
+        display: none;
+    }
+
+    .header-mob-flex {
+        display: flex;
+        flex-direction: column;
+        justify-content: left;
+
+        gap: 20px;
+    }
+
+    .menu {
+        padding: 30px 0;
+    }
+
+    .header-mob-nav-item {
+        display: block;
+        font-family: "Montserrat";
+        text-transform: uppercase;
+
+        color: $text-header;
+        text-decoration: none;
+
+        &:hover {
+            color: $bg;
+        }
+    }
+}
+
+.header-flex {
+
+    padding: 30px 0;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    @media only screen and (max-width: 1279px) {
+        padding: 17px 0;
+    }
+}
+
+.menu-btn {
+    display: none;
+
+    position: relative;
+    width: 30px;
+    height: 30px;
+    border-radius: 4px;
+    cursor: pointer;
+    -webkit-transition: background-color 0.25s;
+    transition: background-color 0.25s;
+
+    background-color: $bg-header;
+
+    @media (max-width: 1280px) {
+        display: block;
+    }
+
+    & > div {
+        position: absolute;
+        left: 7.5px;
+        width: 15px;
+        height: 2px;
+    }
+
+    .part1 {
+        top: 9px;
+        background: $text-header;
+    }
+
+    .part2 {
+        top: 14px;
+        background: $text-header;
+    }
+
+    .part3 {
+        bottom: 9px;
+        background: $text-header;
+    }
+}
+
+.bg-btn {
+    background-color: $bg;
+}
+
+.head-info {
+    display: flex;
+    flex-direction: row;
+    gap: 30px;
+
+    @media (max-width: 1280px) {
+        display: none;
+    }
+}
+
+.side-icon-box {
+    display: flex;
+    flex-direction: row;
+
+    gap: 10px;
+
+    .icon {
+        background-color: $bg;
+        width: 50px;
+        height: 50px;
+
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .content {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+    }
+}
+
+.isVis {
+    display: block;
+}
+
+.header-nav {
+    position: absolute;
+    z-index: 20;
+    width: 100%;
+    max-width: 1180px;
+
+    display: flex;
+    justify-content: center;
+    gap: 40px;
+    background-color: $bg-header;
+    padding: 24.5px 20px;
+    
+
+    @media (max-width: 1280px) {
+        display: none;
+    }
+
+
+    .header-nav-item {
+        font-family: "Montserrat";
+        text-transform: uppercase;
+
+        color: $text-header;
+        text-decoration: none;
+
+        &:hover {
+            color: $bg;
+        }
+    }
+}
+
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,324 @@
+<template>
+    <section class="main-block">
+        <div class="container main-center">
+            <h1>Строительная компания "Воздвижение"</h1>
+            <p>Надежная компания с большим опытом на рынке строительства</p>
+            <nuxt-link to="">Наши проекты</nuxt-link>
+        </div>
+    </section>
+    <section class="main-advantages">
+        <div class="container">
+            <div class="flex-advantages">
+
+                <div class="card" v-for="item in cards">
+                    <div :style="{ backgroundImage: `url(${item.img})` }" class="image">
+                        <div class="icon">
+                            <img :src=" '/logo/' + item.icon " alt="">
+                        </div>
+                    </div>
+                    <div class="content-card">
+                        <h3>{{ item.title }}</h3>
+                        <span>{{ item.text }}</span>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <section class="main-project">
+        <div class="container">
+            <h2>Наши проекты</h2>
+            <div class="project-flex">
+
+                <div class="card-project" v-for="item in project">
+                    <nuxt-link class="card-project-img" to="">
+                        <div :style="{ backgroundImage: `url(${item.img})`}"></div>
+                    </nuxt-link>
+                    <nuxt-link to="">
+                        <h3>{{ item.name }}</h3>
+                    </nuxt-link>
+                </div>
+
+            </div>
+        </div>
+    </section>
+</template>
+
+<script setup>
+const cards = [
+    {
+        icon: 'rub.svg',
+        title: 'финансовая прозрачность',
+        text: 'В основе которой лежат наши приоритеты: ответственность и честность. Доверяя свой проект нам, Вы может быть уверены в рациональном и целевом использовании денежных средст.', 
+        img: '/image/front-advantage-1.jpg'
+    },
+    {
+        icon: 'brick.svg',
+        title: 'качественные  стройматериалы',
+        text: 'Обеспечивают непревзойденный результат и безопасность строительства. Все используемые нами материалы имеют сертификаты соответствия международным стандартам. Мы заботимся о Вас и окружающей среде.',
+        img: '/image/front-advantage-2.jpg'
+    },
+    {
+        icon: 'technique.svg',
+        title: 'собственная техническая база',
+        text: 'Позволяет слаженно и эффективно выполнять поставленные задачи. Отсутствие посредников дает возможность минимизировать издержки и экономит время.',
+        img: '/image/front-advantage-3.jpg'
+    },
+]
+
+const project = [
+    {id: 1, img: '/image/front-advantage-2.jpg', name: 'Пристройка на 200 мест к зданию МОУ СОШ №17'},
+    {id: 2, img: '/image/front-advantage-2.jpg', name: 'Улица Зайцева и Москворецкая набережная'},
+    {id: 3, img: '/image/front-advantage-2.jpg', name: 'ФОК с плавательным бассейном, го Серебряные Пруды'},
+    {id: 4, img: '/image/front-advantage-2.jpg', name: 'Часовня Св. Александра Невского'},
+    {id: 5, img: '/image/front-advantage-2.jpg', name: 'Благоустройство ул. Ремесленной, го Серебряные Пруды, Уч. №5'},
+    {id: 6, img: '/image/front-advantage-2.jpg', name: 'Работы по повышению безопасности дор. движ., РУАД № 5'},
+    {id: 7, img: '/image/front-advantage-2.jpg', name: 'Работы по повышению безопасности дор. движ., РУАД № 5'},
+    {id: 8, img: '/image/front-advantage-2.jpg', name: 'Работы по повышению безопасности дор. движ., РУАД № 5'}
+]
+
+</script>
+
+<style lang="scss">
+.main-block {
+    margin-top: 35px;
+
+    display: block;
+    background: url('/image/front-page-banner-1.jpg') no-repeat center / cover;
+    width: 100%; 
+
+    @media only screen and (max-width: 1279px) {
+        margin-top: 0;
+    }
+
+    .main-center {
+        height: 550px;
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        text-align: center;
+        align-items: center;
+
+        color: $text-header;
+
+        & > h1 {
+            font-family: "Montserrat", "Arial", sans-serif;
+            font-weight: 500;
+            text-transform: uppercase;
+            font-size: 40px;
+
+            @media only screen and (max-width: 767px) {
+                font-size: 28px;
+            }
+        }
+
+        & > p {
+            font-family: "PT Sans";
+            font-size: 18px;
+            font-weight: bold;
+
+            margin-bottom: 20px;
+        }
+
+        & > a {
+            background-color: $bg;
+            color: $text-header;
+
+            display: inline-block;
+            padding: 12px 35px;
+            font-family: "Montserrat", "Arial", sans-serif;
+            font-weight: 500;
+            text-align: center;
+            text-decoration: none;
+            text-transform: uppercase;
+            border-radius: 3px;
+            -webkit-transition: background-color 0.25s;
+            transition: background-color 0.25s;
+            cursor: pointer;
+
+            &:hover {
+                background-color: $bg-hover;
+            }
+        }
+    }
+}
+
+.main-advantages {
+    padding: 70px 0 50px 0;
+
+    background-color: $bg-advantages;
+
+    .flex-advantages {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+
+        gap: 20px;
+
+        width: 100%;
+
+        @media only screen and (max-width: 1279px) {
+            flex-direction: column;
+        }
+
+        .card {
+            background-color: $text-header;
+            text-align: center;
+
+            width: 100%;
+
+            .image {
+                position: relative;
+                height: 250px;
+                margin-bottom: 50px;
+                background-repeat: no-repeat;
+                background-position: center;
+                background-size: cover;
+
+                .icon {
+                    background-color: $bg;
+
+                    position: absolute;
+                    bottom: -40px;
+                    left: calc(50% - 40px);
+                    display: -webkit-box;
+                    display: flex;
+                    -webkit-box-pack: center;
+                    justify-content: center;
+                    -webkit-box-align: center;
+                    align-items: center;
+                    width: 80px;
+                    height: 80px;
+                    border-radius: 50%;
+                    box-shadow: 0 0 9px 2px rgba(0, 0, 0, 0.1);
+                }
+            }
+
+            .content-card {
+                padding: 0 28px 40px 28px;
+
+                h3 {
+                    margin-bottom: 16px;
+                    font-size: 17px;
+                    color: $title;
+                    text-transform: uppercase;
+
+                    &::after {
+                        content: "";
+                        margin-left: calc( 50% - 18.5px);
+                        display: block;
+                        width: 37px;
+                        height: 3px;
+                        margin-top: 16px;
+
+                        background-color: $bg;
+                    }
+                }
+
+                p {
+                    color: $text-color2;
+                }
+            }
+        }
+    }
+}
+
+.main-project {
+    padding-top: 60px;
+
+
+    .project-flex {
+        display: flex;
+        flex-wrap: wrap;
+        width: 100%;
+
+        gap: 20px;
+
+        .card-project {
+            max-width: calc((100% - 20px * 2) / 3);
+            width: 100%;
+
+            .card-project-img {
+                display: block;
+
+                position: relative;
+                height: 270px;
+                margin-bottom: 20px;
+                overflow: hidden;
+    
+                &::before, &::after {
+                    content: "";
+                    position: absolute;
+                    z-index: 10;
+                    top: 0;
+                    left: 0;
+                    display: block;
+                    width: 100%;
+                    height: 100%;
+                }
+    
+                &::before {
+                    background-color: rgba(0, 0, 0, 0);
+                    -webkit-transition: background-color 0.25s;
+                    transition: background-color 0.25s;
+                }
+    
+                &::after {
+                    background-image: url('/logo/chain.svg');
+                    background-repeat: no-repeat;
+                    background-position: center;
+                    opacity: 0;
+                    -webkit-transition: opacity 0.25s;
+                    transition: opacity 0.25s;
+                }
+    
+                &:hover > div {
+                    transform: scale(1.1);
+                }
+    
+                &:hover::before {
+                    background-color: rgba(0, 0, 0, 0.5);
+                }
+    
+                &:hover::after {
+                    opacity: 1;
+                }
+    
+                & > div {
+                    width: 100%;
+                    height: 100%;
+                    background-repeat: no-repeat;
+                    background-position: center;
+                    background-size: cover;
+                    -webkit-transition: -webkit-transform 0.25s;
+                    transition: -webkit-transform 0.25s;
+                    transition: transform 0.25s;
+                    transition: transform 0.25s, -webkit-transform 0.25s;
+                    
+                }
+                
+            }
+
+            a {
+                text-align: center;
+                display: block;
+
+                font-family: "Montserrat";
+
+                &:hover {
+                    text-decoration: none;
+                }
+
+                h3 {
+                    -webkit-transition: color 0.25s;
+                    transition: color 0.25s;
+                    font-size: 17px;
+                    text-transform: uppercase;
+                }
+            }
+
+        }
+
+    }
+}
+</style>
